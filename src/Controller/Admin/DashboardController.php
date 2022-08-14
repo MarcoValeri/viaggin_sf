abbb61e45a1ca067e16b223a59b4981d2ed36c5b
Marco Valeri <marcovaleri@hotmail.it>
2022-08-14 13:49:25 +0100

    Add author entity

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Article;
+use App\Entity\Author;
 use App\Entity\Category;
 use App\Entity\Image;
 use App\Entity\Tag;
@@ -38,23 +39,6 @@ class DashboardController extends AbstractDashboardController
             'articles' => $articles
         ]);
 
-        // return parent::index();
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -72,6 +56,8 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Images', 'fas fa-images', Image::class);
         yield MenuItem::linkToCrud('Categories', 'far fa-newspaper', Category::class);
         yield MenuItem::linkToCrud('Tags', 'fas fa-tags', Tag::class);
+        yield MenuItem::section('Authors');
+        yield MenuItem::linkToCrud('Authors', 'fa fa-pen', Author::class);
         yield MenuItem::section('Users');
         yield MenuItem::linkToUrl('Create User', 'fa-solid fa-face-smile', $this->generateUrl('app_registration_user'));
         yield MenuItem::linkToUrl('Edit Users', 'fa-solid fa-users', $this->generateUrl('app_admin_users'));
