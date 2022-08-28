1e908b1c092d130c482142c1af4fedc8ad77e14b
Marco Valeri <marcovaleri@hotmail.it>
2022-08-28 09:29:08 +0100

    Add author taxonomi

--- src/Controller/AboutUsController.php
+++ src/Controller/AboutUsController.php
@@ -2,14 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\AuthorRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AboutUsController extends AbstractController {
 
     #[Route('/chi-siamo', name: 'app_about-us', priority: 1)]
-    public function aboutUs() {
-        return $this->render('pages/about-us.html.twig');
+    public function aboutUs(AuthorRepository $authorRepository) {
+
+        // Save authors into a variable
+        $getAllAuthors = $authorRepository->findAll();
+
+        return $this->render('pages/about-us.html.twig', [
+            'getAllAuthors'  => $getAllAuthors
+        ]);
     }
 
 }
--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,19 +2,42 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
+use App\Repository\AuthorRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AuthorController extends AbstractController {
 
-    #[Route('/autori', name: 'app_authors', priority: 1)]
-    public function authors() {
-        return $this->render('authors/authors.html.twig');
+    #[Route('/autori/tutti-gli-autori', name: 'app_authors', priority: 1)]
+    public function authors(AuthorRepository $authorRepository, ArticleRepository $articleRepository) {
+
+        // Save authors into a variable
+        $getAllAuthors = $authorRepository->findAll();
+
+        // Save articles into a variable
+        $getAllArticles = $articleRepository->findAll();
+
+        return $this->render('authors/authors.html.twig', [
+            'getAllAuthors'  => $getAllAuthors,
+            'getAllArticles' => $getAllArticles
+        ]);
     }
 
-    #[Route('/autore/marco-valeri', name: 'app_author', priority: 1)]
-    public function author() {
-        return $this->render('authors/author.html.twig');
+    #[Route('/autori/{slug}', name: 'app_author', priority: 1)]
+    public function author(AuthorRepository $authorRepository, ArticleRepository $articleRepository, string $slug) {
+
+        // Save authors into a variable
+        $getAllAuthors = $authorRepository->findAll();
+
+        // Save articles into a variable
+        $getAllArticles = $articleRepository->findAll();
+
+        return $this->render('authors/author.html.twig', [
+            'slug'           => $slug,
+            'getAllAuthors'  => $getAllAuthors,
+            'getAllArticles' => $getAllArticles
+        ]);
     }
 
 }
