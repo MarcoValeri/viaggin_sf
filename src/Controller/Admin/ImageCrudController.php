a6c1468d52652346e103032f899a8ad28824ab39
MarcoValeri <marcovaleri@hotmail.it>
2023-07-15 12:21:34 +0100

    Add the right image path

--- src/Controller/Admin/ImageCrudController.php
+++ src/Controller/Admin/ImageCrudController.php
@@ -18,7 +18,7 @@ class ImageCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            ImageField::new('file_name')->setUploadDir('/public/images')->setBasePath('images'),
+            ImageField::new('file_name')->setUploadDir('/public_html/images')->setBasePath('images'),
             TextField::new('alternative_text'),
             TextField::new('title'),
             TextField::new('caption'),
