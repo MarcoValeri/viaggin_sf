ce826cde289fd4fd143f1f0ec4504f2f255a9680
MarcoValeri <marcovaleri@hotmail.it>
2023-07-22 11:30:09 +0100

    Change file directory to document

--- src/Controller/Admin/DocumentCrudController.php
+++ src/Controller/Admin/DocumentCrudController.php
@@ -2,23 +2,23 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\File;
+use App\Entity\Document;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 
-class FileCrudController extends AbstractCrudController
+class DocumentCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
-        return File::class;
+        return Document::class;
     }
 
     public function configureFields(string $pageName): iterable
     {
         return [
-            ImageField::new('file_name')->setUploadDir('/public/files')->setBasePath('files'),
+            ImageField::new('file_name')->setUploadDir('/public_html/documents')->setBasePath('documents'),
             TextField::new('alternative_text'),
             TextField::new('title'),
             TextField::new('caption'),
--- src/Entity/Document.php
+++ src/Entity/Document.php
@@ -2,11 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\FileRepository;
+use App\Repository\DocumentRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: FileRepository::class)]
-class File
+#[ORM\Entity(repositoryClass: DocumentRepository::class)]
+class Document
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
