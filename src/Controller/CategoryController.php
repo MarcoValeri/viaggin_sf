6be692b3126c1f186ae499b267c7e72de780a171
MarcoValeri <marcovaleri@hotmail.it>
2023-07-23 08:47:17 +0100

    Improve category query

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\ArticleRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
 
 class CategoryController extends AbstractController {
 
@@ -13,48 +14,168 @@ class CategoryController extends AbstractController {
         return $this->render('categories/categories.html.twig');
     }
 
-    #[Route('/category/viaggi', name: 'app_category_viaggi')]
-    public function categoryViaggi(ArticleRepository $articleRepository) {
+    #[Route('/category/viaggi-pagina-{pageNumber}', name: 'app_category_viaggi')]
+    public function categoryViaggi(ArticleRepository $articleRepository, ManagerRegistry $doctrine, string $pageNumber)
+    {
 
-        // Save all the articles into a variable
-        $getArticles = $articleRepository->findAll();
+        $fromArticleNumber = $pageNumber * 10;
+        $sqlQuery = "
+            SELECT
+                article.category_id,
+                article.url,
+                article.title,
+                article.date,
+                image.file_name,
+                image.alternative_text,
+                article.content
+            FROM
+                article
+            INNER JOIN
+                image ON article.image_id = image.id
+            WHERE article.date < NOW()
+            AND article.category_id = 1
+            ORDER BY date DESC
+            LIMIT {$fromArticleNumber}, 10
+        ";
 
-        return $this->render('categories/viaggi.html.twig', [
-            'getArticles' => $getArticles
-        ]);
+        $conn = $doctrine->getConnection();
+        $stmt = $conn->prepare($sqlQuery);
+        $result = $stmt->executeQuery();
+        $articles = $result->fetchAllAssociative();
+
+        if (count($articles) > 0) {
+            return $this->render("categories/viaggi.html.twig", [
+                'articles'      => $articles,
+                'pageNumber'    => $pageNumber
+            ]);
+        } else {
+            return $this->redirectToRoute('app_error404');
+        }
     }
 
-    #[Route('/category/eventi', name: 'app_category_eventi')]
-    public function categoryEventi(ArticleRepository $articleRepository) {
+    #[Route('/category/eventi-pagina-{pageNumber}', name: 'app_category_eventi')]
+    public function categoryEventi(ArticleRepository $articleRepository, ManagerRegistry $doctrine, string $pageNumber)
+    {
 
-        // Save all the articles into a variable
-        $getArticles = $articleRepository->findAll();
+        $fromArticleNumber = $pageNumber * 10;
+        $sqlQuery = "
+            SELECT
+                article.category_id,
+                article.url,
+                article.title,
+                article.date,
+                image.file_name,
+                image.alternative_text,
+                article.content
+            FROM
+                article
+            INNER JOIN
+                image ON article.image_id = image.id
+            WHERE article.date < NOW()
+            AND article.category_id = 3
+            ORDER BY date DESC
+            LIMIT {$fromArticleNumber}, 10
+        ";
 
-        return $this->render('categories/eventi.html.twig', [
-            'getArticles' => $getArticles
-        ]);
+        $conn = $doctrine->getConnection();
+        $stmt = $conn->prepare($sqlQuery);
+        $result = $stmt->executeQuery();
+        $articles = $result->fetchAllAssociative();
+
+        if (count($articles) > 0) {
+            return $this->render("categories/eventi.html.twig", [
+                'articles'      => $articles,
+                'pageNumber'    => $pageNumber
+            ]);
+        } else {
+            return $this->redirectToRoute('app_error404');
+        }
     }
 
-    #[Route('/category/documenti', name: 'app_category_documenti')]
-    public function categoryDocumenti(ArticleRepository $articleRepository) {
+    #[Route('/category/documenti-pagina-{pageNumber}', name: 'app_category_documenti')]
+    public function categoryDocumenti(ArticleRepository $articleRepository, ManagerRegistry $doctrine, string $pageNumber)
+    {
 
-        // Save all the articles into a variable
-        $getArticles = $articleRepository->findAll();
+        $fromArticleNumber = $pageNumber * 10;
+        $sqlQuery = "
+            SELECT
+                article.category_id,
+                article.url,
+                article.title,
+                article.date,
+                image.file_name,
+                image.alternative_text,
+                article.content
+            FROM
+                article
+            INNER JOIN
+                image ON article.image_id = image.id
+            WHERE article.date < NOW()
+            AND article.category_id = 2
+            ORDER BY date DESC
+            LIMIT {$fromArticleNumber}, 10
+        ";
 
-        return $this->render('categories/documenti.html.twig', [
-            'getArticles' => $getArticles
-        ]);
+        $conn = $doctrine->getConnection();
+        $stmt = $conn->prepare($sqlQuery);
+        $result = $stmt->executeQuery();
+        $articles = $result->fetchAllAssociative();
+
+        if (count($articles) > 0) {
+            return $this->render("categories/documenti.html.twig", [
+                'articles'      => $articles,
+                'pageNumber'    => $pageNumber
+            ]);
+        } else {
+            return $this->redirectToRoute('app_error404');
+        }
+        
     }
 
-    #[Route('/category/vivere-estero', name: 'app_category_estero')]
-    public function categoryEstero(ArticleRepository $articleRepository) {
+    #[Route('/category/vivere-estero-pagina-{pageNumber}', name: 'app_category_estero')]
+    public function categoryEstero(ArticleRepository $articleRepository, ManagerRegistry $doctrine, string $pageNumber)
+    {
+
+        $fromArticleNumber = $pageNumber * 10;
+        $sqlQuery = "
+            SELECT
+                article.category_id,
+                article.url,
+                article.title,
+                article.date,
+                image.file_name,
+                image.alternative_text,
+                article.content
+            FROM
+                article
+            INNER JOIN
+                image ON article.image_id = image.id
+            WHERE article.date < NOW()
+            AND article.category_id = 4
+            ORDER BY date DESC
+            LIMIT {$fromArticleNumber}, 10
+        ";
+
+        $conn = $doctrine->getConnection();
+        $stmt = $conn->prepare($sqlQuery);
+        $result = $stmt->executeQuery();
+        $articles = $result->fetchAllAssociative();
+
+        if (count($articles) > 0) {
+            return $this->render("categories/vivere-estero.html.twig", [
+                'articles'      => $articles,
+                'pageNumber'    => $pageNumber
+            ]);
+        } else {
+            return $this->redirectToRoute('app_error404');
+        }
 
         // Save all the articles into a variable
-        $getArticles = $articleRepository->findAll();
+        // $getArticles = $articleRepository->findAll();
 
-        return $this->render('categories/vivere-estero.html.twig', [
-            'getArticles' => $getArticles
-        ]);
+        // return $this->render('categories/vivere-estero.html.twig', [
+        //     'getArticles' => $getArticles
+        // ]);
     }
 
 }
