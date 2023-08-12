0021aa1628087ce0596f5ce440a6032f6893fd7b
MarcoValeri <marcovaleri@hotmail.it>
2023-08-12 20:22:26 +0100

    Create sitemap for images

--- src/Controller/SitemapController.php
+++ src/Controller/SitemapController.php
@@ -2,19 +2,34 @@
 
 namespace App\Controller;
 
-use App\Repository\ArticleRepository;
 use App\Repository\AuthorRepository;
 use App\Repository\TagRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SitemapController extends AbstractController {
 
     #[Route('/sitemap.xml', name: "app_sitemap", defaults: ["_format" => "xml"], priority: 1)]
-    public function sitemap(ArticleRepository $articleRepository, AuthorRepository $authorRepository, TagRepository $tagRepository) {
+    public function sitemap(AuthorRepository $authorRepository, TagRepository $tagRepository, ManagerRegistry $doctrine)
+    {
 
         // Save all the articles into a variable
-        $articles = $articleRepository->findAll();
+        $sqlQuery = "
+            SELECT
+                article.url,
+                article.date,
+                article.update_at
+            FROM
+                article
+            WHERE article.date < NOW()
+            ORDER BY article.date DESC
+        ";
+
+        $conn = $doctrine->getConnection();
+        $stmt = $conn->prepare($sqlQuery);
+        $result = $stmt->executeQuery();
+        $articles = $result->fetchAllAssociative();
 
         // Save all the authors into a variables
         $authors = $authorRepository->findAll();
@@ -28,6 +43,31 @@ class SitemapController extends AbstractController {
             'tags'      => $tags
         ]);
 
+    }
+
+    #[Route('/sitemap_images.xml', name: "app_sitemap_image", defaults: ["_format" => "xml"], priority: 1)]
+    public function sitemapImage(ManagerRegistry $doctrine)
+    {
+        // Save all images into a variable
+        $sqlQuery = "
+            SELECT
+                image.file_name,
+                image.title,
+                image.description
+            FROM
+                image
+            ORDER BY image.id DESC
+        ";
+
+        $conn = $doctrine->getConnection();
+        $stmt = $conn->prepare($sqlQuery);
+        $result = $stmt->executeQuery();
+        $images = $result->fetchAllAssociative();
+        // dd($images);
+
+        return $this->render("seo/sitemap-images.html.twig", [
+            'images' => $images,
+        ]);
     }
 
 }
